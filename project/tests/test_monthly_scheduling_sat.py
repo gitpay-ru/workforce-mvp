a58ff8d769005c0c3631cbaedfa36c1db35018e6
Aleksandr Davydenko <alexandrdavydenko@yandex.ru>
2023-01-10 12:30:22 +0300

    New tests added, should force OOM exception and protobuf 2gb limitation

--- project/tests/test_monthly_scheduling_sat.py
+++ project/tests/test_monthly_scheduling_sat.py
@@ -50,6 +50,22 @@ def test_happy_path_1_week():
 
     model.solve()
 
+def test_oom_1_week_350employees():
+    _days = 7
+    _1d_intervals = int(24 * 60 / 15)
+
+    model = MonthlyShiftScheduling(
+        num_employees=350,
+        num_intervals=_1d_intervals*_days,
+        intervals_demand=[10 for _ in range(_1d_intervals*_days)],
+        fixed_assignments=[],
+        employee_requests=[],
+        employee_stop_list=[],
+        employee_schemas=[]
+    )
+
+    model.solve()
+
 def test_happy_path_1_month():
     _days = 31
     _intervals = _days * int(24 * 60 / 15)
@@ -58,6 +74,22 @@ def test_happy_path_1_month():
         num_employees=20,
         num_intervals=_intervals,
         intervals_demand=[3 for _ in range(_intervals)],
+        fixed_assignments=[],
+        employee_requests=[],
+        employee_stop_list=[],
+        employee_schemas=[]
+    )
+
+    model.solve()
+
+def test_oom_1_month_350employees():
+    _days = 31
+    _intervals = _days * int(24 * 60 / 15)
+
+    model = MonthlyShiftScheduling(
+        num_employees=350,
+        num_intervals=_intervals,
+        intervals_demand=[10 for _ in range(_intervals)],
         fixed_assignments=[],
         employee_requests=[],
         employee_stop_list=[],
@@ -134,6 +166,23 @@ def test_feasible_light_6_days():
 def test_feasible_light_7_days():
     # starting from a week a new constraint to be applied - min/max working hours
     _days = 7
+    _1d_intervals = int(24 * 60 / 15)
+
+    model = MonthlyShiftScheduling(
+        num_employees=6,
+        num_intervals=_1d_intervals*_days,
+        intervals_demand=[1 for _ in range(_1d_intervals*_days)],
+        fixed_assignments=[],
+        employee_requests=[],
+        employee_stop_list=[],
+        employee_schemas=[]
+    )
+
+    model.solve()
+
+def test_feasible_light_1_month():
+    # starting from a week a new constraint to be applied - min/max working hours
+    _days = 31
     _1d_intervals = int(24 * 60 / 15)
 
     model = MonthlyShiftScheduling(
