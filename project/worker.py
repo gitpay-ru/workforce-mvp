bf7cb1fdeb0b0c8b89649d628980fbbe45b6f762
Vladimir Kocheryzhkin <vkocherizhkin@gmail.com>
2023-01-15 22:48:35 +0300

    Enable output dir

--- project/worker.py
+++ project/worker.py
@@ -6,6 +6,7 @@ import json
 import numpy as np
 import pandas as pd
 import math
+from pathlib import Path
 from pyworkforce.rostering.binary_programming import MinHoursRoster
 from pyworkforce.scheduling import MinAbsDifference
 
@@ -95,127 +96,27 @@ def calculate_achieved_stats(shift_names, rostering_solution, df_csv):
     return df_csv
 
 @celery.task(name="create_task")
-def create_task():#shift_names, num_resources, min_working_hours, max_resting):
+def create_task():
 
-    # 0. Load data
-    shutil.copyfile("./tmp/data", f'./tmp/{current_task.request.id}')
-    shutil.copyfile("./tmp/meta", f'./tmp/{current_task.request.id}_meta')
+    output_dir = f'./tmp/{current_task.request.id}'
+    Path(output_dir).mkdir(parents=True, exist_ok=True)
 
-    input_csv_path = './tmp/{current_task.request.id}'
-    input_meta_path = './tmp/{current_task.request.id}_meta'
-    output_dir = '..'
-    mzp = MultiZonePlanner(input_csv_path, input_meta_path, output_dir)
+    shutil.copyfile("./tmp/data", f'./tmp/{current_task.request.id}/input')
+    shutil.copyfile("./tmp/meta", f'./tmp/{current_task.request.id}/meta')
+
+    input_csv_path = f'./tmp/{current_task.request.id}/input'
+    input_meta_path = f'./tmp/{current_task.request.id}/meta'
+
+    df = pd.read_csv(input_csv_path, parse_dates=[0], index_col=0)
+    with open(input_meta_path, 'r', encoding='utf-8') as f:
+        meta = json.load(f)
+
+    # print(df)
+
+    mzp = MultiZonePlanner(df, meta, output_dir)
     mzp.schedule()
     mzp.roster()
     mzp.roster_postprocess()
-    exit()
-
-    with open(f'./tmp/{current_task.request.id}_meta', "r") as f:
-        meta_info = json.load(f)
-
-    HMin = 60
-    DayH = 24
-    # todo: fix hardcoded Days number
-    NDays = 31
-    shift_names = meta_info['shift_names'].split(',')
-    shifts_coverage = get_shift_coverage(shift_names)
-
-    # 1. Calculate required positions, based on input .csv
-    df = pd.read_csv(f'./tmp/{current_task.request.id}')
-    df['positions'] = df.apply(lambda row: required_positions(row['call_volume'], row['aht'], 15, row['art'], row['service_level']), axis=1)
-
-    min_date = get_datetime(min(df['tc']))
-    max_date = get_datetime(max(df['tc']))
-    days = (max_date - min_date).days + 1
-    date_diff = get_datetime(df.iloc[1]['tc']) - get_datetime(df.iloc[0]['tc'])
-    step_min = int(date_diff.total_seconds() / HMin)
-    
-    ts = int(HMin / step_min)
-
-    df['target_positions'] = df['positions']   # persist positions for future use
-    ratio = 1.0          # this is a starting resource scaling -> trying to schedule with required coverage
-
-    # downgrade resource requirements on each iteration according to the configured percentage
-    # on every failed operation will be reduce required resource by some % of original
-    resources_degradation = list(meta_info['resources_degradation'])
-    for ratio in resources_degradation:
-        print(f'==================================')
-        print(f'Solving with positions ratio = {ratio}')
-        df['positions'] = df['target_positions'].apply(lambda x: int(x*ratio))
-
-        required_resources = []
-        for i in range(days):
-            df0 = df[i * DayH * ts : (i + 1) * DayH * ts]
-            required_resources.append(df0['positions'].tolist())
-
-        print('Scheduling started')
-        scheduler = MinAbsDifference(num_days = days,  # S
-                                     periods = DayH * ts,  # P
-                                     shifts_coverage = shifts_coverage,
-                                     required_resources = required_resources,
-                                     max_period_concurrency = int(df['positions'].max()),  # gamma
-                                     max_shift_concurrency=int(df['positions'].mean()),  # beta
-                                     )
-        sch_solution = scheduler.solve()
-
-        with open(f'./tmp/{current_task.request.id}_scheduling.json', 'w') as f:
-            json.dump(sch_solution, f, indent=2)
-
-        status = sch_solution['status']
-        print(f'Scheduling status: {status}')
-
-        if status not in ['OPTIMAL', 'FEASIBLE']:
-            return False
-
-        resources = meta_info['resources']
-        shift_names = list(shifts_coverage.keys())
-        shifts_hours = [int(i.split('_')[1]) for i in shift_names]
-
-        resources_shifts = sch_solution['resources_shifts']
-        df1 = pd.DataFrame(resources_shifts)
-        df2 = df1.pivot(index='shift', columns='day', values='resources').rename_axis(None, axis=0)
-        df2['combined']= df2.values.tolist()
-        required_resources = df2['combined'].to_dict()
-
-        banned_shifts = []
-        non_sequential_shifts = []
-        resources_preferences = []
-        resources_prioritization = []
-
-        min_working_hours = meta_info['min_working_hours']
-        max_resting = meta_info['max_resting']
-
-        solver = MinHoursRoster(num_days=days,
-            resources=resources,
-            shifts=shift_names,
-            shifts_hours=shifts_hours,
-            min_working_hours=min_working_hours,
-            max_resting=max_resting,
-            non_sequential_shifts=non_sequential_shifts,
-            banned_shifts=banned_shifts,
-            required_resources=required_resources,
-            resources_preferences = resources_preferences,
-            resources_prioritization = resources_prioritization
-            )
-
-        solution = solver.solve()
-
-        status = solution['status']
-        print(f'Rostering status: {status}')
-
-        if status in ['OPTIMAL', 'FEASIBLE']:
-            with open(f'./tmp/{current_task.request.id}_rostering.json', 'w') as outfile:
-                json.dump(solution, outfile, indent=2)
-
-            print(f'Calculating achieved statistics')
-            df_out = calculate_achieved_stats(shift_names, solution, df)
-
-            _output_csv_filename = f'./tmp/{current_task.request.id}_achieved_stats'
-            df_out.to_csv(_output_csv_filename)
-
-            print(f'Done')
-
-            return True
 
     return True
 
