17384e9c46b30a2c4c0506837ee0e1936cd64047
Vladimir Kocheryzhkin <vkocherizhkin@gmail.com>
2023-02-01 19:19:00 +0300

    refactor state tracking

--- project/main.py
+++ project/main.py
@@ -1,13 +1,9 @@
 from celery.result import AsyncResult
-from fastapi import Body, FastAPI, File, UploadFile
-from typing import List, Optional, Union
+from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from celery import current_task
 from fastapi.responses import StreamingResponse
-from fastapi.encoders import jsonable_encoder
-from celery.task.control import revoke
 from pathlib import Path
 
 from worker import create_task, terminate_task
@@ -15,10 +11,6 @@ from worker import create_task, terminate_task
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
-
-def task_exists(id):
-    t = Path(f'./tmp/{id}')
-    return t.exists()
 
 def iterfile(path):
     with open(path, mode="rb") as file_like:
@@ -41,37 +33,38 @@ def submit_task(
 
 @app.get("/task/{id}/status")
 def get_task_status(id):
-
-    if not task_exists(id):
+    try:
+        task_result = AsyncResult(id)
+        result = {
+            "id": id,
+            "status": task_result.status,
+            "result": task_result.result
+        }
+        return JSONResponse(result)
+    except:
         return JSONResponse(status_code=404)
-
-    task_result = AsyncResult(id)
-    result = {
-        "id": id,
-        "status": task_result.status,
-        "result": task_result.result
-    }
-    return JSONResponse(result)
 
 
 @app.get("/task/{id}/result")
 def get_schedule_result(id):
-    if not task_exists(id):
+    fpath = f'./tmp/{id}/rostering.json'
+    if Path(fpath).exists():
+        return StreamingResponse(iterfile(fpath), media_type = "application/octet-stream")
+    else:
         return JSONResponse(status_code=404)
-
-    return StreamingResponse(iterfile(f'./tmp/{id}/rostering.json'), media_type = "application/octet-stream")
-
 
 @app.get("/task/{id}/statistics-results")
 def get_stats_result(id):
-    if not task_exists(id):
+    fpath = f'./tmp/{id}/statistics_output.json'
+    if Path(fpath).exists():
+        return StreamingResponse(iterfile(), media_type="application/octet-stream")
+    else:
         return JSONResponse(status_code=404)
-    return StreamingResponse(iterfile(f'./tmp/{id}/statistics_output.json'), media_type="application/octet-stream")
 
 @app.get("/task/{id}/cancel")
 def cancel_task(id):
-    if not task_exists(id):
+    try:
+        res = terminate_task.delay(id)
+        return JSONResponse({"id": res.id})
+    except:
         return JSONResponse(status_code=404)
-
-    terminate_task.delay(id)
-    return JSONResponse({"id": id})
