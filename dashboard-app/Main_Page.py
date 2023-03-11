395b2ce3541bcc46e1ba298e39322f886d69b4ea
adavydenko <alexandrdavydenko@yandex.ru>
2023-03-11 09:46:29 +0300

    Layout updated, minor fixes

--- dashboard-app/Main_Page.py
+++ dashboard-app/Main_Page.py
@@ -1,43 +1,26 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="WFM Dashboard",
+    page_title="WFM Reports",
     page_icon="📈",
 )
 
-# sidebar
 st.sidebar.success("Start analysis by selecting report above.")
 # just for testing - how metric are looked like
-m = st.sidebar.metric("Temperature", "70 °F", "1.2 °F")
+# m = st.sidebar.metric("Temperature", "70 °F", "1.2 °F")
 
-# page
+st.write("# Welcome to WFM Reports 📈")
 
-st.write("# Welcome to WFM Dashboard 📈")
-
-col1, col2, col3 = st.columns(3)
-col1.metric("Total Employees", "375")
-col3.metric("Target SL", "80%", "4%")
-col2.metric("Target ART", "9 sec", "-8%")
+# col1, col2, col3 = st.columns(3)
+# col1.metric("Total Employees", "375")
+# col2.metric("Target SL", "80%", "4%")
+# col3.metric("Target ART", "9 sec", "-8%")
 
 st.markdown(
     """
-    Streamlit is an open-source app framework built specifically for
+    Reporting app is based on a Streamlit -- an open-source app framework built specifically for
     Machine Learning and Data Science projects.
 
-    **👈 Select a demo from the dropdown on the left** to see some examples
-    of what Streamlit can do!
-
-    ### Want to learn more?
-
-    - Check out [streamlit.io](https://streamlit.io)
-    - Jump into our [documentation](https://docs.streamlit.io)
-    - Ask a question in our [community
-      forums](https://discuss.streamlit.io)
-
-    ### See more complex demos
-
-    - Use a neural net to [analyze the Udacity Self-driving Car Image
-      Dataset](https://github.com/streamlit/demo-self-driving)
-    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+    **👈 Select a report from the left** to see some nice visualizations
 """
 )
