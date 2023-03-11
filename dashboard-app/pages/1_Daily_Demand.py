f74f43d79b4917d0757fa0e84ff1ef4eda21b3a1
adavydenko <alexandrdavydenko@yandex.ru>
2023-03-11 09:39:36 +0300

    Report updated, tabs added

--- dashboard-app/pages/1_Daily_Demand.py
+++ dashboard-app/pages/1_Daily_Demand.py
@@ -2,17 +2,47 @@ import streamlit as st
 import plost
 import pandas as pd
 
-csv_file = st.sidebar.file_uploader("Upload input .csv file")
-if csv_file is not None:
+json_file = st.sidebar.file_uploader("Upload statistics .json file")
+if json_file is not None:
     # Can be used wherever a "file-like" object is accepted:
-    df = pd.read_csv(csv_file)
+    df = pd.read_json(json_file)
     df['tc'] = pd.to_datetime(df['tc'])
 
-    plost.time_hist(
-        data=df,
-        date='tc',
-        x_unit='hours',
-        y_unit='date',
-        color='call_volume',
-        aggregate='mean',
+    st.write(
+        """
+        This graph shows corresponding values spreaded across the month. Mean values are aggregated into hours.
+        """
     )
+
+    tab1, tab2, tab3 = st.tabs(["Call volume", "Required Positions", "Scheduled Positions"])
+
+    with tab1:
+        plost.time_hist(
+            data=df,
+            date='tc',
+            x_unit='hours',
+            y_unit='date',
+            color='call_volume',
+            aggregate='mean',
+        )
+
+    with tab2:
+        plost.time_hist(
+            data=df,
+            date='tc',
+            x_unit='hours',
+            y_unit='date',
+            color='positions',
+            aggregate='mean',
+        )
+
+    with tab3:
+        plost.time_hist(
+            data=df,
+            date='tc',
+            x_unit='hours',
+            y_unit='date',
+            color='scheduled_positions',
+            aggregate='mean',
+        )
+
