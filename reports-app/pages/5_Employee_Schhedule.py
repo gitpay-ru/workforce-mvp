69d5e1e7becd3cdb2abfaf2004f46b669758106b
Aleksandr Davydenko <alexandrdavydenko@yandex.ru>
2023-03-27 11:51:55 +0300

    wip

--- reports-app/pages/5_Employee_Schhedule.py
+++ reports-app/pages/5_Employee_Schhedule.py
@@ -1,68 +1,41 @@
+import json
 import streamlit as st
 import plotly.express as px
+import plotly.graph_objects as go
 import pandas as pd
-import json
 import datetime
-import plost
-
-st.set_page_config(
-    page_title="Schedule summary",
-    page_icon="📈",
-)
-
-st.header("Schedule summary")
-
-col1, col2, col3 = st.columns(3)
-
-st.write(
-    """
-    This graph illustrates a combination of resource shifts & breaks assigned per employee. 
-    for shifts and breaks the following rules are applied:
-     - shifts: min/max shifts per month
-     - shifts: min/max wordays & holdiays in row
-     - breaks: number of breaks per shift
-     - breaks: min/max start time per break
-     - min/max intervals between breaks
-     
-    You can use a full-screen view to investigate results in details.
-    """
-)
-
-status_text = st.sidebar.empty()
-status_text.markdown("**Waiting for files upload**")
 
 rostering_file = st.sidebar.file_uploader("Upload 'rostering.json' file: ")
 meta_file = st.sidebar.file_uploader("Upload 'meta_file.json' file: ")
 if rostering_file is not None and meta_file is not None:
 
-    status_text.markdown("**Start processing ..**")
-
     rostering = json.load(rostering_file)
     meta = json.load(meta_file)
 
-    num_employees = len(meta['employees'])
-    col1.metric("Total Employees", num_employees)
-
     shifts_duration = {}
     for s in meta['shifts']:
-        shifts_duration[s['id']] = datetime.timedelta(minutes= int(s['duration'][:-3]) * 60+ int(s['duration'][-2:]))
+        shifts_duration[s['id']] = datetime.timedelta(minutes=int(s['duration'][:-3]) * 60 + int(s['duration'][-2:]))
 
     # shifts to dataframe
     format = '%d.%m.%y %H:%M'
     shifts = map(
-            lambda x: dict(Employee = str(x['employeeId']),
-                           Start = datetime.datetime.strptime(f"{x['shiftDate']} {x['shiftTimeStart']}", format),
-                           Finish = datetime.datetime.strptime(f"{x['shiftDate']} {x['shiftTimeStart']}", format) + shifts_duration[x['shiftId']],
-                           Activity = x['shiftId'],
-                           width = 0.4)
-            , rostering['campainSchedule']
+        lambda x: dict(Employee=str(x['employeeId']),
+                       Start=datetime.datetime.strptime(f"{x['shiftDate']} {x['shiftTimeStart']}", format),
+                       Finish=datetime.datetime.strptime(f"{x['shiftDate']} {x['shiftTimeStart']}", format) +
+                              shifts_duration[x['shiftId']],
+                       Activity=x['shiftId'],
+                       width=0.4)
+        , rostering['campainSchedule']
     )
 
-    # breaks
+    employee_id = st.text_input('Enter Employee ID for details')
+    st.write('Selected employee: ', employee_id)
+
     df_shifts = pd.DataFrame(shifts)
 
-    num_shifts = len(df_shifts)
-    col2.metric("Shifts scheduled", num_shifts)
+    col1, col2, col3 = st.columns(3)
+    col1.metric("Total Employees", len(meta['employees']))
+    col2.metric("Shifts scheduled", len(df_shifts))
 
     # breaks to dataframe
     breaks = []
@@ -86,11 +59,11 @@ if rostering_file is not None and meta_file is not None:
                 activity_end += datetime.timedelta(days=1)
 
             breaks.append(
-                dict(Employee = str(employee_id),
-                     Start = activity_start,
-                     Finish = activity_end,
-                     Activity = activity_id,
-                     width = 0.6)
+                dict(Employee=str(employee_id),
+                     Start=activity_start,
+                     Finish=activity_end,
+                     Activity=activity_id,
+                     width=0.6)
             )
 
     df_breaks = pd.DataFrame(breaks)
@@ -100,15 +73,13 @@ if rostering_file is not None and meta_file is not None:
 
     df = pd.concat([df_shifts, df_breaks])
 
-
-
     with st.expander("See explanation"):
         df
 
     status_text.markdown("**Preparing graph ..**")
 
     # Plotly!
-    fig = px.timeline(df, x_start="Start", x_end="Finish", y="Employee", color="Activity", height=num_employees*16)
+    fig = px.timeline(df, x_start="Start", x_end="Finish", y="Employee", color="Activity", height=num_employees * 16)
     fig.update_yaxes(autorange="reversed", visible=False, showticklabels=False)
     fig.update_layout(showlegend=False)
     for i, d in enumerate(fig.data):
@@ -125,3 +96,4 @@ if rostering_file is not None and meta_file is not None:
 # this button is not connected to any other logic, it just causes a plain
 # rerun.
 # st.button("Re-run")
+
