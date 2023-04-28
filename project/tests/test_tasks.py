311b20954d733790e4a0118021b2b54d7e98e256
Kocheryzhkin Vladimir <vkocherizhkin@gmail.com>
2023-04-28 09:12:44 +0300

    Lock pandas version for worker. v1.1.3 (#20)

--- project/tests/test_tasks.py
+++ project/tests/test_tasks.py
@@ -1,48 +1,62 @@
+import pytest
+import requests
 import json
+import csv
+import time
+from datetime import datetime
+from datetime import timedelta
+import os
+from dotenv import load_dotenv, find_dotenv
+import pandas as pd
 
-from unittest.mock import patch, call
+def test_time():
+    load_dotenv(find_dotenv())
 
-from worker import create_task
+    with open(f'./_data_file_imp.csv', 'r', encoding='utf-8') as f:
+        reader = csv.reader(f)
 
+    with open(f'./_meta_one_person_176h.json', 'r', encoding='utf-8') as f:
+        meta = json.load(f)
 
-def test_home(test_app):
-    response = test_app.get("/")
-    assert response.status_code == 200
+    with open(f'./_solver_profile_file.json', 'r', encoding='utf-8') as f:
+        solver = json.load(f)
 
+    files = {
+        "data_file": open('_data_file_imp.csv', 'rb'),
+        "meta_file": open('_meta_one_person_176h.json', 'rb'),
+        "solver_profile_file": open('_solver_profile_file.json', 'rb')
+    }
 
-def test_task():
-    assert create_task.run(1)
-    assert create_task.run(2)
-    assert create_task.run(3)
+    res = requests.post(os.getenv('urlpost'), files=files)
+    time.sleep(8)
 
+    id = (res.json()['id'])
+    op = f'task/{id}/result'
+    sos = os.getenv('urlget')
+    response = requests.get((sos) + str(op))
 
-@patch("worker.create_task.run")
-def test_mock_task(mock_run):
-    assert create_task.run(1)
-    create_task.run.assert_called_once_with(1)
+    shifts_duration = {}
+    for s in meta['shifts']:
+        shifts_duration[s['id']] = timedelta(minutes=int(s['duration'][:-3]) * 60 + int(s['duration'][-2:]))
 
-    assert create_task.run(2)
-    assert create_task.run.call_count == 2
-
-    assert create_task.run(3)
-    assert create_task.run.call_count == 3
-
-
-def test_task_status(test_app):
-    response = test_app.post(
-        "/tasks",
-        data=json.dumps({"type": 1})
+    format = '%d.%m.%y %H:%M'
+    shifts = map(
+        lambda x: dict(Employee=str(x['employeeId']),
+                       StartDateTime=datetime.strptime(f"{x['shiftDate']} {x['shiftTimeStart']}", format),
+                       FinishDateTeme=datetime.strptime(f"{x['shiftDate']} {x['shiftTimeStart']}", format) +
+                                      shifts_duration[x['shiftId']],
+                       Shift=x['shiftId'])
+        , response.json()["campainSchedule"]
     )
-    content = response.json()
-    task_id = content["task_id"]
-    assert task_id
 
-    response = test_app.get(f"tasks/{task_id}")
-    content = response.json()
-    assert content == {"task_id": task_id, "task_status": "PENDING", "task_result": None}
-    assert response.status_code == 200
+    minWorkingHours = meta['employees'][0]['minWorkingHours']
+    maxWorkingHours = meta['employees'][0]['maxWorkingHours']
 
-    while content["task_status"] == "PENDING":
-        response = test_app.get(f"tasks/{task_id}")
-        content = response.json()
-    assert content == {"task_id": task_id, "task_status": "SUCCESS", "task_result": True}
+    actual_time = len(list(shifts)) * 9 - 22
+
+    assert actual_time <= maxWorkingHours
+    assert actual_time >= minWorkingHours
+
+    # with open('res.json', 'w') as f:
+    #     json.dump(response.json(), f)
+
