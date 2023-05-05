53536c84cc251b85963d10bbada9f862945dc712
RomaPetuhov <petuhov.roma@mail.ru>
2023-05-05 11:16:40 +0300

    rework and add tests

--- project/tests/test_tasks.py
+++ project/tests/test_tasks.py
@@ -17,7 +17,7 @@ def test_actual_time():
     with open(f'./_data_file_imp.csv', 'r', encoding='utf-8') as f:
         reader = csv.reader(f)
 
-    with open(f'./_meta_file.json', 'r', encoding='utf-8') as f:
+    with open(f'./_meta_file_first.json', 'r', encoding='utf-8') as f:
         meta = json.load(f)
 
     with open(f'./_solver_profile_file.json', 'r', encoding='utf-8') as f:
@@ -25,18 +25,17 @@ def test_actual_time():
 
     files = {
         "data_file": open('_data_file_imp.csv', 'rb'),
-        "meta_file": open('_meta_file.json', 'rb'),
+        "meta_file": open('_meta_file_first.json', 'rb'),
         "solver_profile_file": open('_solver_profile_file.json', 'rb')
     }
 
     res = requests.post(os.getenv('urlpost'), files=files)
-    time.sleep(8)
+    time.sleep(20)
 
     id = (res.json()['id'])
     op = f'task/{id}/result'
     sos = os.getenv('urlget')
     response = requests.get((sos) + str(op))
-
 
     shifts_duration = {}
     for s in meta['shifts']:
@@ -56,7 +55,7 @@ def test_actual_time():
 
     expected_time = 9 * 22  # 198
 
-    actual_time = len(list(shifts)) * 9
+    actual_time = len(list(shifts)) * 3
 
     assert int(actual_time) == int(expected_time)
 
@@ -66,7 +65,7 @@ def test_actual_time_v2():
     with open(f'./_data_file_imp.csv', 'r', encoding='utf-8') as f:
         reader = csv.reader(f)
 
-    with open(f'./_meta_file.json', 'r', encoding='utf-8') as f:
+    with open(f'./_meta_file_first.json', 'r', encoding='utf-8') as f:
         meta = json.load(f)
 
     with open(f'./_solver_profile_file.json', 'r', encoding='utf-8') as f:
@@ -74,12 +73,12 @@ def test_actual_time_v2():
 
     files = {
         "data_file": open('_data_file_imp.csv', 'rb'),
-        "meta_file": open('_meta_file.json', 'rb'),
+        "meta_file": open('_meta_file_first.json', 'rb'),
         "solver_profile_file": open('_solver_profile_file.json', 'rb')
     }
 
     res = requests.post(os.getenv('urlpost'), files=files)
-    time.sleep(8)
+    time.sleep(20)
 
     id = (res.json()['id'])
     op = f'task/{id}/result'
@@ -93,12 +92,12 @@ def test_actual_time_v2():
 
 
     expected_time = 198
-    actual_time = (df['employeeId'].count() * 9)
+    actual_time = (df['employeeId'].count() * 3)
 
     assert int(actual_time) == int(expected_time)
 
 
-def test_time():
+def test_first_employees_working_hours():
     load_dotenv(find_dotenv())
 
     with open(f'./_data_file_imp.csv', 'r', encoding='utf-8') as f:
@@ -117,13 +116,12 @@ def test_time():
     }
 
     res = requests.post(os.getenv('urlpost'), files=files)
-    time.sleep(8)
+    time.sleep(20)
 
     id = (res.json()['id'])
     op = f'task/{id}/result'
     sos = os.getenv('urlget')
     response = requests.get((sos) + str(op))
-
 
     shifts_duration = {}
     for s in meta['shifts']:
@@ -140,8 +138,58 @@ def test_time():
                        Shift=x['shiftId'])
         , response.json()["campainSchedule"]
     )
-    expected_time = 9 * 10  # 90
 
-    actual_time = len(list(shifts)) * 4.1
+    minWorkingHours = meta['employees'][0]['minWorkingHours']
+    maxWorkingHours = meta['employees'][0]['maxWorkingHours']
 
-    assert int(actual_time) == int(expected_time)
+    actual_time = len(list(shifts)) * 9 - 22
+
+    assert actual_time <= maxWorkingHours
+    assert actual_time >= minWorkingHours
+
+
+def test_shift_time_start():
+    load_dotenv(find_dotenv())
+
+    with open(f'./_data_file_imp.csv', 'r', encoding='utf-8') as f:
+        reader = csv.reader(f)
+
+    with open(f'./_meta_file_first.json', 'r', encoding='utf-8') as f:
+        meta = json.load(f)
+
+    with open(f'./_solver_profile_file.json', 'r', encoding='utf-8') as f:
+        solver = json.load(f)
+
+    files = {
+        "data_file": open('_data_file_imp.csv', 'rb'),
+        "meta_file": open('_meta_file_first.json', 'rb'),
+        "solver_profile_file": open('_solver_profile_file.json', 'rb')
+    }
+
+    res = requests.post(os.getenv('urlpost'), files=files)
+    time.sleep(20)
+
+    id = (res.json()['id'])
+    op = f'task/{id}/result'
+    sos = os.getenv('urlget')
+    response = requests.get((sos) + str(op))
+
+    response_dict = response.json()
+    df = pd.DataFrame(response_dict)
+    df['employeeId'] = df.apply(lambda t: t['campainSchedule']['employeeId'], axis=1)
+    df['shiftId'] = df.apply(lambda t: t['campainSchedule']['shiftId'], axis=1)
+    df['shiftTimeStart'] = df.apply(lambda t: t['campainSchedule']['shiftTimeStart'], axis=1)
+
+    shiftId = df['shiftId'][0]
+    shiftTimeStart = df['shiftTimeStart'][0]
+
+
+    scheduleTimeStart = meta['shifts'][0]['scheduleTimeStart']
+    scheduleTimeEndStart = meta['shifts'][0]['scheduleTimeEndStart']
+    id = meta['shifts'][0]['id']
+
+
+
+    assert shiftId == id
+    assert shiftTimeStart >= scheduleTimeStart
+    assert shiftTimeStart <= scheduleTimeEndStart
