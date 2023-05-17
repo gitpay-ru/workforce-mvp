c1954ea581bc8b1b0954a7577fb3ee74c9bbbe21
adavydenko <alexandrdavydenko@yandex.ru>
2023-05-18 01:11:56 +0300

    default settings changed

--- reports-app/Main_Page.py
+++ reports-app/Main_Page.py
@@ -3,6 +3,7 @@ import streamlit as st
 st.set_page_config(
     page_title="WFM Reports",
     page_icon="📈",
+    layout="wide"
 )
 
 st.sidebar.success("Start analysis by selecting report above.")
