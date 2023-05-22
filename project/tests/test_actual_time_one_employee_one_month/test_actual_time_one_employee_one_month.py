8f719c113c49951ffb431a518720418e1639f3d4
Vladimir Kocheryzhkin <vkocherizhkin@gmail.com>
2023-05-22 14:36:05 +0300

    test fixup

--- project/tests/test_actual_time_one_employee_one_month/test_actual_time_one_employee_one_month.py
+++ project/tests/test_actual_time_one_employee_one_month/test_actual_time_one_employee_one_month.py
@@ -8,21 +8,8 @@ from datetime import timedelta
 import os
 from dotenv import load_dotenv, find_dotenv
 
-format = '%d.%m.%y %H:%M'
-
-with open('test_actual_time_one_employee_one_month/_data_file_improvisation.csv', 'r', encoding='utf-8') as f:
-    reader = csv.reader(f)
-
-with open(f'test_actual_time_one_employee_one_month/_meta_file_actual_time_one_employee_one_month.json', 'r',
-          encoding='utf-8') as f:
-    meta = json.load(f)
-
-with open('test_actual_time_one_employee_one_month/_solver_profile_file.json', 'r', encoding='utf-8') as f:
-    solver = json.load(f)
-
-
-def response_to_shifts(response):
-
+def response_to_shifts(meta, response):
+    format = '%d.%m.%y %H:%M'
     shifts_duration = {}
     for s in meta['shifts']:
         shifts_duration[s['id']] = timedelta(minutes=int(s['duration'][:-3]) * 60 + int(s['duration'][-2:]))
@@ -40,18 +27,25 @@ def response_to_shifts(response):
 def test_actual_time_one_employee_one_month():
     load_dotenv(find_dotenv())
 
+    #Arrange
     files = {
-        'data_file': (str(reader), 'rb'),
-        'meta_file': (str(meta), 'rb'),
-        'solver_profile_file': (str(solver), 'rb')
+        'data_file': open('test_actual_time_one_employee_one_month/_data_file_improvisation.csv', 'rb'),
+        'meta_file': open('test_actual_time_one_employee_one_month/_meta_file_actual_time_one_employee_one_month.json', 'rb'),
+        'solver_profile_file': open('test_actual_time_one_employee_one_month/_solver_profile_file.json', 'rb')
     }
+    with open(f'test_actual_time_one_employee_one_month/_meta_file_actual_time_one_employee_one_month.json', 'r',
+          encoding='utf-8') as f:
+        meta = json.load(f)
+
+    #Act
     res = requests.post(os.getenv('urlpost'), files=files)
     time.sleep(50)
 
+
+    #Assert
     id = (res.json()['id'])
     response = requests.get(os.getenv('urlget') + f'task/{id}/result')
-    response_to_shifts(response)
-    shifts = response_to_shifts(response)
+    shifts = response_to_shifts(meta, response)
 
     expected_time = 198
     actual_time = len(list(shifts)) * 3
